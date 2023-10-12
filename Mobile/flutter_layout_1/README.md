5a60b801a1979b1100baafd71019243e5df4c14e
Jauhar Maulana <jauharmaulana4546@gmail.com>
2023-10-12 09:01:04 +0700

    Minggu 7

---
+++ Mobile/flutter_layout_1/README.md
@@ -0,0 +1,75 @@
+# flutter_layout_1
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+## Praktikum 1
+> Membangun Layout di FLutter
+
+### Langkah 1
+> Buat sebuah project baru dengan nama ***flutter_layout_1*** lalu Ubah code program pada ***main.dart***
+<img src='screenshot/1-2.png' width="300px">
+
+### Langkah 2
+> Tambahkan kode berikut di bagian atas metode ***build()*** di dalam kelas ***MyApp***
+<img src='screenshot/1.png' width="300px">
+
+### Hasil Akhir
+<img src='screenshot/1-1.png' width="300px">
+
+## Praktikum 2
+> Implementasi Button Row
+
+### Langkah 1
+> Membuat method Column ***_buildButtonColumn***
+
+Membuat metode pembantu pribadi bernama buildButtonColumn(), yang mempunyai parameter warna, Icon dan Text, sehingga dapat mengembalikan kolom dengan widgetnya sesuai dengan warna tertentu.
+<img src='screenshot/2.png' width="300px">
+
+### Langkah 2
+> Code Program
+
+<img src='screenshot/2-2.png' width="300px">
+
+### Hasil Akhir
+
+<img src='screenshot/2-1.png' width="300px">
+
+## Praktikum 3
+>Implementasi Text Section
+
+### Langkah 1 
+> Code program
+
+<img src='screenshot/3-2.png' width="300px">
+
+### Hasil Akhir
+<img src='screenshot/3-1.png' width="300px">
+
+## Praktikum 4
+>Implementasi Image Section
+
+### Langkah 1 
+> Menyiapkan Aset Gambar
+
+> Code program
+
+<img src='screenshot/4-1.png' width="300px">
+
+### Langkah 2
+> Menambahakan Gambar ke Body
+
+> Code program
+
+<img src='screenshot/4-2.png' width="300px">
+
+### Hasil Akhir
+<img src='screenshot/END.png' width="300px">
---
+++ Mobile/flutter_layout_1/lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Kuil Kiyomizu-Dera',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Higashi-yama, Kyoto, Jepang',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /* soal 3*/
+          Icon(
+          Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        "Jauhar Maulana A'la, Malang, Indonesia\n\n"
+        'Di Kyoto Jepang, tempat wisata memang didominasi dengan tempat wisata berupa kuil. Salah satu kuil yang bisa dikunjungi saat ada disini adalah Kuil Kiyomizu.Kuil ini dibangun pada tahun 1633, nama Kiyamizu sendiri mengambil dari nama air terjun yang mengalir di tebing bukit Kyoto. Arti kiyamizu adalah murni atau suci. Setidaknya Kyoto memiliki sekitar 200 kuil baik besar maupun kecil dan kuil Kiyomizu merupakan kuil utama dan terbesar yang ada di Kyoto. Bangunan di kuil ini terbuat dari kayu dan ada beranda luas yang ditopang oleh kayu berbentuk pilar. Di bawah kuil ini ada pemandangan yang hijau dan ada pepohonan yang rimbun. Kuil ini merupakan kuil Buddha dan termasuk kuil dengan bangunan kuo. Kuil ini telah beberapa kali mengalami kerusakan. Tidak kurang dari 10 kali kuil ini mengalami kerusakan yang terbakar akibat perang maupun dikarenakan bencana alam yang terjadi di Jepang. Saat wisatawan berada di belakang kuil, wisatawan akan menemukan kuil bernama Jishu – Jinja dimana kuil itu dijadikan sebagai kuil dengan dewa perjodohan.',
+        style: TextStyle(
+          color: Colors.black87),
+        textAlign: TextAlign.justify,
+        softWrap: true,
+        ),
+      );
+    Color color =  Theme.of(context).primaryColor;
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+    return MaterialApp(
+      title: "Jauhar Maulana A'la, 2141720816",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+              // Image.asset(
+              //   'images/kuil-kiyomizu.jpg',
+              // width: 600,
+              // height: 240,
+              // fit: BoxFit.cover,),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+    Column _buildButtonColumn(Color color, IconData icon, String label) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+}
